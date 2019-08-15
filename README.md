e4c10f0d30f73d0941360049839397dce8f13498
MarkusZP <markslavenkoo@ukr.net>
2019-08-15 13:11:24 +0300

    c1

---
+++ README.md
@@ -0,0 +1,5 @@
+## Start
+`npm install`
+`node name_of_file.js`
+## Notes
+Doesn't work perfect. Google blocks likes, after 3-4. Ideally, embed VPN or proxy here.
---
+++ likes to comment sync.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer');
+
+const mails = ['mail1', 'mail2', '...']; // gmail
+const passwords = ['pass1', 'pass2', '...'];
+const link_to_comment = 'link to comment';
+
+async function main(log, pass) {
+	for (let i = 0; i < mails.length; i++) {
+  	const browser = await puppeteer.launch({headless: true}); //change to false, if you want to see progress
+    const page = await browser.newPage();
+    page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+    await page.goto('https://accounts.google.com/ServiceLogin?hl=ru&passive=true&continue=https://www.google.com/');
+    await page.setViewport({width: 1000, height: 1000})
+    await page.waitFor('input[name=identifier]');
+    await page.$eval('input[name=identifier]', (el, value) => el.value = value, mails[i]);
+    await page.click('#identifierNext');
+
+    await page.waitFor('input[name=password]');
+    await page.$eval('input[name=password]', (el, value) => el.value = value, passwords[i]);
+    await page.waitFor(2000);
+    await page.click('#passwordNext'); 
+
+    await page.waitFor(2000);
+    await page.goto(link_to_comment);
+    await page.waitFor('#like-button > a');
+    await page.waitFor(2000);
+    await page.click('#like-button > a');
+    console.log('Like set!')
+
+    await page.waitFor(1000);
+    await browser.close();
+	}
+}
+
+main();
